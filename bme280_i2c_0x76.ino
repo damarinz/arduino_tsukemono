887e2b6193785502e0e6d4a563acd8fcb4d72bc4
Bungo Tamari <damarinz@gmail.com>
2018-05-07 21:55:44 +0900

    Create bme280_i2c_0x76.ino

---
+++ bme280_i2c_0x76.ino
@@ -0,0 +1,36 @@
+#include "SparkFunBME280.h"
+#include "Wire.h"
+#include "SPI.h"
+
+BME280 Sensor;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Sensor.settings.commInterface = I2C_MODE; 
+  Sensor.settings.I2CAddress = 0x76;
+  Sensor.settings.runMode = 3; 
+  Sensor.settings.tStandby = 0;
+  Sensor.settings.filter = 0;
+  Sensor.settings.tempOverSample = 1 ;
+  Sensor.settings.pressOverSample = 1;
+  Sensor.settings.humidOverSample = 1;
+
+  Serial.println("Starting BME280... ");
+  delay(10);
+  Sensor.begin();
+}
+
+void loop() {
+  Serial.print("Temperature: ");
+  Serial.print(Sensor.readTempC(), 2);
+  Serial.print(" °C");
+  Serial.print("\t Pressure: ");
+  Serial.print(Sensor.readFloatPressure() / 100, 2);
+  Serial.print(" hPa");
+  Serial.print("\t Humidity: ");
+  Serial.print(Sensor.readFloatHumidity(), 2);
+  Serial.println(" %");
+  delay(1000);
+}
